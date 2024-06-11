9e21d4ca598b46232c78f5c95c0cf375a3cdc08a
Francisco A. Myers M. <franciscomyers@gmail.com>
2024-06-11 13:31:32 -0400

    feat: implement service

--- src/payments/payments.controller.ts
+++ src/payments/payments.controller.ts
@@ -1,17 +1,23 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, Req, Res } from '@nestjs/common';
 import { PaymentsService } from './payments.service';
+import { PaymentSessionDto } from './dto/payment-session.dto';
+import { Request, Response } from 'express';
 
 @Controller('payments')
 export class PaymentsController {
   constructor(private readonly paymentsService: PaymentsService) {}
+
   @Post('create-payment-session')
-  createPaymentSession() {
-    return 'createPaymentSession';
+  createPaymentSession(@Body() paymentSessionDto: PaymentSessionDto) {
+    return this.paymentsService.createPaymentSession(paymentSessionDto);
   }
 
   @Get('success')
   success() {
-    return 'Payment successful';
+    return {
+      ok: true,
+      message: 'Payment successful',
+    };
   }
 
   @Get('cancel')
@@ -23,7 +29,7 @@ export class PaymentsController {
   }
 
   @Post('webhook')
-  async stripeWebhook() {
-    return 'stripeWebhook';
+  async stripeWebhook(@Req() req: Request, @Res() res: Response) {
+    return this.paymentsService.stripeWebhook(req, res);
   }
 }
