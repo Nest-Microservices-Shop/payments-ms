94b1db7ac48168040ea4167bb43462882c5aabb4
Francisco A. Myers M. <franciscomyers@gmail.com>
2024-06-11 13:28:35 -0400

    feat: added variables

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -4,11 +4,19 @@ import * as joi from 'joi';
 
 interface EnvVars {
   PORT: number;
+  STRIPE_SECRET: string;
+  STRIPE_ENDPOINT_SECRET: string;
+  STRIPE_CANCEL_URL: string;
+  STRIPE_SUCCESS_URL: string;
 }
 
 const envSchema = joi
   .object({
     PORT: joi.number().required(),
+    STRIPE_SECRET: joi.string().required(),
+    STRIPE_ENDPOINT_SECRET: joi.string().required(),
+    STRIPE_CANCEL_URL: joi.string().required(),
+    STRIPE_SUCCESS_URL: joi.string().required(),
   })
   .unknown(true);
 
@@ -22,4 +30,8 @@ const envVars: EnvVars = value;
 
 export const envs = {
   port: envVars.PORT,
+  stripeSecret: envVars.STRIPE_SECRET,
+  stripeEndpointSecret: envVars.STRIPE_ENDPOINT_SECRET,
+  stripeCancelUrl: envVars.STRIPE_CANCEL_URL,
+  stripeSuccessUrl: envVars.STRIPE_SUCCESS_URL,
 };
